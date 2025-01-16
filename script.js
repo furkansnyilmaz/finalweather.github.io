8568440f4ff331541a337ef2f5322dea55196f6f
furkansnyilmaz <56487130+furkansnyilmaz@users.noreply.github.com>
2025-01-16 20:07:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,266 @@
+const notFavDurum = "<svg id='svg1' width='15px' height='15px' viewBox='0 0 32 32' enable-background='new 0 0 32 32' version='1.1' xml:space='preserve' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><path d='  M16.842,3.548l3.29,6.984c0.137,0.29,0.401,0.491,0.707,0.538l7.357,1.12c0.77,0.117,1.077,1.108,0.52,1.677l-5.324,5.436  c-0.221,0.226-0.322,0.551-0.27,0.87l1.257,7.676c0.131,0.803-0.673,1.416-1.362,1.036l-6.58-3.624c-0.273-0.151-0.6-0.151-0.873,0  l-6.58,3.624c-0.688,0.379-1.493-0.233-1.362-1.036l1.257-7.676c0.052-0.319-0.049-0.644-0.27-0.87l-5.324-5.436  c-0.557-0.569-0.25-1.56,0.52-1.677l7.357-1.12c0.306-0.047,0.57-0.248,0.707-0.538l3.29-6.984  C15.503,2.817,16.497,2.817,16.842,3.548z' fill='none' id='XMLID_16_' stroke='currentcolor' stroke-linecap='round' stroke-linejoin='round' stroke-miterlimit='10' stroke-width='2'/></svg>";
+const favDurum = "<svg id='svg2' fill='currentcolor' width='15px' height='15px' viewBox='0 0 32 32' enable-background='new 0 0 32 32' version='1.1' xml:space='preserve' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><path d='M29.895,12.52c-0.235-0.704-0.829-1.209-1.549-1.319l-7.309-1.095l-3.29-6.984C17.42,2.43,16.751,2,16,2  s-1.42,0.43-1.747,1.122l-3.242,6.959l-7.357,1.12c-0.72,0.11-1.313,0.615-1.549,1.319c-0.241,0.723-0.063,1.507,0.465,2.046  l5.321,5.446l-1.257,7.676c-0.125,0.767,0.185,1.518,0.811,1.959c0.602,0.427,1.376,0.469,2.02,0.114l6.489-3.624l6.581,3.624  c0.646,0.355,1.418,0.311,2.02-0.114c0.626-0.441,0.937-1.192,0.811-1.959l-1.259-7.686l5.323-5.436  C29.958,14.027,30.136,13.243,29.895,12.52z' id='XMLID_328_'/></svg>";
+const favDurumIptal = "<svg id='svg3' fill='currentcolor' width='15px' height='15px' viewBox='0 0 32 32' enable-background='new 0 0 32 32' version='1.1' xml:space='preserve' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><path d='M29.707,29.707c-0.391,0.391-1.023,0.391-1.414,0l-26-26c-0.391-0.391-0.391-1.023,0-1.414  s1.023-0.391,1.414,0l7.458,7.458l3.089-6.629C14.58,2.43,15.249,2,16,2s1.42,0.43,1.747,1.122l3.29,6.984l7.309,1.095  c0.72,0.11,1.313,0.615,1.549,1.319c0.241,0.723,0.063,1.507-0.465,2.046l-5.323,5.436l0.527,3.218l5.073,5.073  C30.098,28.684,30.098,29.316,29.707,29.707z' id='XMLID_332_'/><path d='M6.464,10.773c0.036-0.006,0.073,0.007,0.099,0.033l18.298,18.298c0.069,0.069,0.076,0.187,0.01,0.259  c-0.618,0.683-1.572,0.818-2.336,0.398l-6.581-3.624l-6.489,3.624c-0.644,0.354-1.418,0.312-2.02-0.114  c-0.626-0.441-0.937-1.192-0.811-1.959l1.257-7.676L2.57,14.566c-0.528-0.539-0.706-1.323-0.465-2.046  c0.235-0.704,0.829-1.209,1.549-1.319L6.464,10.773z' id='XMLID_334_'/></svg>";
+const notFavDurumIptal = "<svg id='svg4' width='15px' height='15px' viewBox='0 0 32 32' enable-background='new 0 0 32 32' version='1.1' xml:space='preserve' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><path d='  M16.842,3.548l3.29,6.984c0.137,0.29,0.401,0.491,0.707,0.538l7.357,1.12c0.77,0.117,1.077,1.108,0.52,1.677l-5.324,5.436  c-0.221,0.226-0.322,0.551-0.27,0.87l1.257,7.676c0.131,0.803-0.673,1.416-1.362,1.036l-6.58-3.624c-0.273-0.151-0.6-0.151-0.873,0  l-6.58,3.624c-0.688,0.379-1.493-0.233-1.362-1.036l1.257-7.676c0.052-0.319-0.049-0.644-0.27-0.87l-5.324-5.436  c-0.557-0.569-0.25-1.56,0.52-1.677l7.357-1.12c0.306-0.047,0.57-0.248,0.707-0.538l3.29-6.984  C15.503,2.817,16.497,2.817,16.842,3.548z' fill='none' id='XMLID_17_' stroke='currentcolor' stroke-linecap='round' stroke-linejoin='round' stroke-miterlimit='10' stroke-width='2'/><line fill='none' id='XMLID_18_' stroke='currentcolor' stroke-linecap='round' stroke-linejoin='round' stroke-miterlimit='10' stroke-width='2' x1='3' x2='29' y1='3' y2='29'/></svg>";
+
+const weatherIcon = ["thunderstorm.png", "windy-rain.png", "windy-rain.png", "thunderstorm.png", "thunderstorm.png"
+    , "snow-heavy.png", "sleet.png", "rainy-snow.png", "drizzle.png", "drizzle.png", "rain.png", "rain.png"
+    , "rain-heavy.png", "snow-heavy.png", "snow-heavy.png", "snow-heavy.png", "snow-heavy.png", "thunderstorm.png"
+    , "hail.png", "dusty.png", "foggy.png", "foggy.png", "smog.png", "windy.png", "windy.png", "freezing.png", "cloud.png"
+    , "cloudy-night-1.png", "cloudy-day-1.png", "cloudy-night.png", "cloudy-day.png", "night.png", "sunny.png"
+    , "night.png", "sunny.png", "sunny.png", "thunderstorm.png", "warm.png", "thunders.png", "thunderstorm-light.png"
+    , "rain-light.png", "rain-heavy.png", "snow-heavy.png", "snow-heavy.png", "windy-snow.png", "cloudy-day.png"
+    , "rain-light.png", "snow-heavy.png", "thunderstorm-1.png"];
+const weatherApiKey = "e1f10a1e78da46f5b10a1e78da96f525"
+const geocodeApiKey = "sPLSVnMaIExYpUZfQvFW9YNQiR5QoEXi"
+let favoriHavaObject = [];
+let aramaHavaObject = [];
+
+function changeActive(element){
+    const rem_element = document.getElementsByClassName("active");
+
+    if (rem_element[0]!==undefined){
+        rem_element[0].classList.remove("active");
+    }
+    element.classList.add("active");
+
+    mainHavaDurum(document.getElementById(element.id+"-sehir").innerHTML,element.id.replace("sb",""));
+}
+
+function weatherURL(cityLat,cityLong,weatherApiKey, sehir){
+    let text="https://api.weather.com/v3/wx/observations/current?geocode=cityLat,cityLong&format=json&units=e&apiKey=weatherApiKey&language=tr-TR";
+    let newtext = text.replace("cityLat",cityLat);
+    newtext = newtext.replace("cityLong",cityLong);
+    newtext = newtext.replace("weatherApiKey",weatherApiKey);
+    let sehirtext = sehir;
+    return [newtext, sehirtext];
+}
+
+function geocodeURL(geocodeApiKey, sehir){
+    let text="https://api.geocodify.com/v2/geocode?api_key=apiKey&q=sehir";
+    let newtext = text.replace("apiKey",geocodeApiKey);
+    newtext = newtext.replace("sehir",sehir);
+    return newtext;
+}
+
+async function geoGet(url){
+    return await fetch(url)
+        .then((response)=>response.json())
+        .then((responseJson)=>{return weatherURL(responseJson.response.features[0].geometry.coordinates[1],responseJson.response.features[0].geometry.coordinates[0],weatherApiKey, responseJson.response.features[0].properties.name)});
+}
+
+async function objGet(url){
+    return await fetch(url)
+        .then((response)=>response.json())
+        .then((responseJson)=>{return responseJson});
+}
+
+async function degerAl(url){
+    const data = await this.geoGet(url);
+    const data1 = await objGet(data[0]);
+    return  [data1, data[1]];
+}
+
+function havaDurumIkon(iconCode){
+    return weatherIcon[iconCode];
+}
+
+async function mainHavaDurum(sehir, index){
+    durum = favoriHavaObject[index];
+
+    havadurum = durum.wxPhraseLong;
+    derece = Math.round((5/9)*((await durum.temperature)-32))+"°C";
+    image = havaDurumIkon(durum.iconCode);
+    airpressure = durum.pressureMeanSeaLevel+" mBar";
+    humidity = durum.relativeHumidity+"%";
+    feeltemp = Math.round((5/9)*((await durum.temperatureFeelsLike)-32))+"°C";
+    uv = durum.uvDescription+"/ "+durum.uvIndex;
+    visibility = durum.visibility+" km";
+    windspeed = durum.windSpeed+" km/s";
+    winddirection = durum.windDirectionCardinal.slice(-2);
+
+    document.getElementById("solbar-image").innerHTML = "<img src='icons/image'>".replace("image",image);
+    document.getElementById("solbar-sehir").innerHTML = sehir;
+    document.getElementById("solbar-havadurumu").innerHTML = havadurum;
+    document.getElementById("solbar-derece").innerHTML= derece;
+    document.getElementById("solbar-airpressure-value").innerHTML= airpressure;
+    document.getElementById("solbar-nem-value").innerHTML = humidity;
+    document.getElementById("solbar-hissicak-value").innerHTML = feeltemp;
+    document.getElementById("solbar-uv-value").innerHTML = uv;
+    document.getElementById("solbar-vis-value").innerHTML = visibility;
+    document.getElementById("solbar-wind-value").innerHTML = windspeed;
+    document.getElementById("solbar-winddir-value").innerHTML = winddirection;
+
+}
+
+async function favoriHavaDurum(sehir1){
+    deger = geocodeURL(geocodeApiKey,sehir1);
+    deger2 = await degerAl(deger);
+    havadurum1 = deger2[0].wxPhraseLong;
+    derece1 = Math.round((5/9)*((await deger2[0].temperature)-32))+"°C";
+    image1 = havaDurumIkon(deger2[0].iconCode);
+    image = "<img src='icons/image'>".replace("image",image1);
+    sehir = deger2[1];
+
+    const favorihavadurumu = {"sehir": sehir, "havadurum": havadurum1, "derece": derece1, "image": image, "object": deger2[0]};
+
+    return favorihavadurumu;
+
+}
+
+function ana(){
+    element = document.getElementsByClassName("active");
+
+    if(element[0]!==undefined){
+        sehir = document.getElementById(element[0].id+"-sehir").innerHTML;
+    }
+    else{
+        changeActive(document.getElementById("sb1"));
+        ana();
+    }
+
+    mainHavaDurum(sehir, element[0].id.replace("sb",""));
+}
+
+async function yeniFavori(sehir, number) {
+    const favoriHavaDurumuListe = await favoriHavaDurum(sehir);
+
+    const favori = document.createElement("div");
+    favori.classList.add("sbclass");
+    favori.classList.add("favori");
+    favori.setAttribute("onclick", "changeActive(this)")
+    favori.setAttribute("id", "sb" + number);
+
+
+    const favori_image = document.createElement("div");
+    favori_image.innerHTML = favoriHavaDurumuListe.image;
+    favori_image.classList.add("sbimage");
+    favori_image.setAttribute("id", "sb" + number + "-image");
+    favori.appendChild(favori_image);
+
+
+    const favori_sehir = document.createElement("span");
+    favori_sehir.innerHTML = favoriHavaDurumuListe.sehir;
+    favori_sehir.classList.add("sbsehir");
+    favori_sehir.setAttribute("id", "sb" + number + "-sehir");
+    favori.appendChild(favori_sehir);
+
+    const favori_derece = document.createElement("span");
+    favori_derece.innerHTML = favoriHavaDurumuListe.derece;
+    favori_derece.classList.add("sbderece");
+    favori_derece.setAttribute("id", "sb"+number+"-derece");
+    favori.appendChild(favori_derece);
+
+    const favori_havadurumu = document.createElement("span");
+    favori_havadurumu.innerHTML = favoriHavaDurumuListe.havadurum;
+    favori_havadurumu.classList.add("sbhavadurumu");
+    favori_havadurumu.setAttribute("id", "sb"+number+"-havadurumu");
+    favori.appendChild(favori_havadurumu);
+
+    const form_element = document.createElement("form");
+    form_element.setAttribute("action","index.php");
+    form_element.setAttribute("method","post");
+    favori.appendChild(form_element);
+
+    const favori_ikon = document.createElement("button");
+    favori_ikon.innerHTML = favDurum;
+    favori_ikon.classList.add("sbikon");
+    favori_ikon.setAttribute("id", "sb"+number+"-ikon");
+    form_element.appendChild(favori_ikon);
+    favori_ikon.setAttribute("type", "submit");
+    favori_ikon.setAttribute("name", "sb-remove-button");
+    favori_ikon.setAttribute("value",favoriHavaDurumuListe.sehir);
+    favori_ikon.setAttribute("onclick","fileJSON(this)")
+
+    document.getElementById("sb").appendChild(favori);
+    return favoriHavaDurumuListe.object;
+}
+
+function fileJSON(element){
+    //remove(element);
+}
+
+function elementSil(element){
+    element.remove();
+}
+
+async function favoriSırala(favoriler){
+    let i=1
+    for(let fav in favoriler){
+        favoriHavaObject[i] = await yeniFavori(favoriler[fav],i);
+        i=i+1;
+    }
+    ana();
+}
+
+async function aramaGoruntule(){
+    let searchResult = await favoriHavaDurum(document.querySelector('#aramakutusu').value);
+
+    const arama = document.createElement("div");
+    arama.setAttribute("id", "arama_sonuc");
+
+    const arama_image = document.createElement("div");
+    arama_image.innerHTML = searchResult.image;
+    arama_image.setAttribute("id", "arama_image");
+    arama.appendChild(arama_image);
+
+    const arama_sehir = document.createElement("span");
+    arama_sehir.innerHTML = searchResult.sehir;
+    arama_sehir.setAttribute("id", "arama_sehir");
+    arama.appendChild(arama_sehir);
+
+    const arama_derece = document.createElement("span");
+    arama_derece.innerHTML = searchResult.derece;
+    arama_derece.setAttribute("id", "arama_derece");
+    arama.appendChild(arama_derece);
+
+    const arama_havadurumu = document.createElement("span");
+    arama_havadurumu.innerHTML = searchResult.havadurum;
+    arama_havadurumu.setAttribute("id", "arama_havadurumu");
+    arama.appendChild(arama_havadurumu);
+
+    const arama_form = document.createElement("form");
+    arama_form.setAttribute("action","index.php");
+    arama_form.setAttribute("method","post");
+    arama.appendChild(arama_form);
+
+    const arama_ikon = document.createElement("button");
+    arama_ikon.innerHTML = notFavDurum;
+    arama_ikon.setAttribute("id", "arama_ikon");
+    arama_ikon.setAttribute("name","sb-add-button");
+    arama_ikon.setAttribute("value",searchResult.sehir);
+    arama_form.appendChild(arama_ikon);
+
+    document.getElementById("overlayid").appendChild(arama);
+    aramaHavaObject[0] = searchResult.object;
+}
+
+function aramaKapatma(){
+    if (document.getElementById("arama_sonuc") !== null){
+        elementSil(document.getElementById("arama_sonuc"));
+    }
+    if (document.getElementById("transparent_background") !== null){
+        elementSil(document.getElementById("transparent_background"));
+    }
+}
+
+function aramaYapma(element){
+    if ((event.key === 'Enter') || (element.nodeName === "BUTTON")){
+        aramaKapatma();
+        const arkaplan = document.createElement("div");
+        arkaplan.setAttribute("id", "transparent_background");
+        arkaplan.setAttribute("onclick", "aramaKapatma()");
+        document.getElementById("overlayid").appendChild(arkaplan);
+
+        aramaGoruntule();
+    }
+}
+
+async function getFile(){
+    favoriler = await fetch("favoriler.json").then(file => file.json());
+    favoriSırala(favoriler);
+}
+
+window.onload = function () {
+    getFile();
+};
